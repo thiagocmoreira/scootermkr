174066371983ab42ea84930defcf9820bb0b5345
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-08-20 17:37:59 -0300

    Adding animation for elements of all pages

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$(".rslides").responsiveSlides({
+$('.rslides').responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
   speed: 500,            // Integer: Speed of the transition, in milliseconds
   timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
@@ -15,11 +15,38 @@ $(".rslides").responsiveSlides({
   namespace: "rslides",   // String: Change the default namespace used
   before: function(){},   // Function: Before callback
   after: function(){}     // Function: After callback
-});
+})
 
-$(".rslides-portfolio").responsiveSlides({
+$('.rslides-portfolio').responsiveSlides({
   auto: true,             // Boolean: Animate automatically, true or false
   speed: 500,            // Integer: Speed of the transition, in milliseconds
   timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
   pager: true,           // Boolean: Show pager, true or false
-});
+})
+
+Visibility.onVisible(function(){
+  // Index
+  setTimeout(function(){
+    $('.introduction h1').addClass('animated fadeInDown')
+  }, 400)
+  setTimeout(function(){
+    $('.introduction blockquote').addClass('animated fadeInDown')
+  }, 800)
+  setTimeout(function(){
+    $('.introduction .btn').addClass('animated fadeInDown')
+  }, 1200)
+  setTimeout(function(){
+    $('.animate').addClass('animated fadeInDown')
+  }, 1600)
+
+  // Other Pages
+  setTimeout(function(){
+    $('.introduction-internal h1').addClass('animated fadeInDown')
+  }, 400)
+  setTimeout(function(){
+    $('.introduction-internal p').addClass('animated fadeInDown')
+  }, 800)
+  setTimeout(function(){
+    $('.animate-internal').addClass('animated fadeInDown')
+  }, 1200)
+})
