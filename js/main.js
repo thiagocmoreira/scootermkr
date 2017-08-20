31e550abbd7b11629dfd8f21a0be9183faa7cf3a
Thiago Moreira <thiago.cmoreira@yahoo.com.br>
2017-08-20 16:03:50 -0300

    Adjusting slide selectors and other minor changes

--- js/main.js
+++ js/main.js
@@ -16,3 +16,10 @@ $(".rslides").responsiveSlides({
   before: function(){},   // Function: Before callback
   after: function(){}     // Function: After callback
 });
+
+$(".rslides-portfolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+});
